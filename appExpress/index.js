08d0dbafd425088138ef2ef18bba494ad5f29a21
rafacalera <rafa_calera@hotmail.com>
2023-04-25 20:41:08 -0300

    Aula 2504 - Middleware e Parâmetros em URL

--- appExpress/index.js
+++ appExpress/index.js
@@ -5,8 +5,28 @@ const path = require('path')
 
 const basePath = path.join(__dirname, 'templates') // abre o diretório templates
 
-app.get('/', (req, res) => {
+var checkAuth = function (req, res, next){ // cria o middleware
+    req.authStatus = true
+    if (req.authStatus) {
+        console.log('Está logado pode continuar')
+        next()
+    }
+    else{
+        console.log('Não está logado! Travado no sistema.')
+    }
+}
+
+app.use(checkAuth) // puxa o middleware
+
+app.get('/', (req, res) => { 
     res.sendFile(`${basePath}/index.html`)
+})
+
+app.get('/user/:id', (req, res) => {
+    var usuario = req.params.id + "gmail.com"
+
+    console.log(`O Navegador mandou: ${usuario} como usuário`)
+    res.sendFile(`${basePath}/user.html`)
 })
 
 app.listen(port, () => {
